f6b233008d64befcc81ae741c65543da65e86e52
alireza <alirezamashayekhy2000@gmail.com>
2023-02-21 14:38:29 +0330

    persian about us page created

--- pages/aboutUs.vue
+++ pages/aboutUs.vue
@@ -197,137 +197,145 @@
         class="capabilities d-flex justify-content-between align-items-center"
       >
         <div>
-          <h2>Our Capabilities</h2>
+          <h2>فعالیت های ما</h2>
           <ul>
             <li>
               <span>
-                Production of all kinds of extruded aluminum sections (special,
-                industrial, construction)
+                تولید انواع مقاطع آلومینیومی اکسترود شده (ویژه، صنعتی، ساختمانی)
               </span>
             </li>
             <li>
               <span>
-                Sale of billets, all kinds of alloy ingots and raw materials
-                related to the aluminum industry
+                تولید انواع مقاطع آلومینیومی اکسترود شده (ویژه، صنعتی، ساختمانی)
               </span>
             </li>
             <li>
               <span>
-                Production and sale of all kinds of sheets in different
-                dimensions and thickness according to customer's order
+                تولید و فروش انواع ورق در ابعاد و ضخامت های مختلف به سفارش
+                مشتری</span
+              >
+            </li>
+            <li>
+              <span>
+                طراحی و ساخت انواع قالب های اکستروژن منطبق با سفارش مشتری.
               </span>
             </li>
             <li>
               <span>
-                Designing and manufacturing all kinds of industrial, special
-                molds and extrusion orders
+                ریخته گری بیلت با کوره کج شونده به روشDC (سایز بیلت از 5 اینچ
+                الی ۸ اینچ در آلیاژهای 1000 تا 7000)
               </span>
             </li>
             <li>
-              <span>
-                DC tilting furnace billet casting (billet size from 5 inches to
-                8 inches in alloys 1000 to 7000)
-              </span>
-            </li>
-            <li>
-              <span> Ingot casting in alloys 1000 to 7000 </span>
+              <span> ریخته گری شمش در آلیاژهای 1000 تا 7000 </span>
             </li>
           </ul>
         </div>
         <img src="/images/capabilities.jpg" alt="" />
       </div>
-      <div class="activities d-flex justify-content-between align-items-center">
+      <div class="activities d-flex justify-content-between align-items-start">
         <img src="/images/activities.jpg" alt="" />
         <div>
-          <h2>Our Activities</h2>
+          <h2>توانمندی های ما</h2>
           <ul>
             <li>
-              <span>
-                Production of all kinds of exclusive aluminum extrusion
-                sections, industrial, construction).
-              </span>
+              <span> صنایع پتروشیمی و پالایشکاه </span>
+            </li>
+            <li>
+              <span> صنایع حمل و نقل ریلی </span>
+            </li>
+            <li>
+              <span> صنایع سرمایشی و گرمایشی ، تهویه </span>
             </li>
             <li>
               <span>
-                Sale of billets of all kinds of alloy ingots and raw materials
-                related to the aluminum industry.
+                صنایع برق و الکترونیک ، انرژی های تجدیدپذیر و مخابرات
               </span>
             </li>
             <li>
-              <span>
-                Supplying and selling all kinds of sheets in different sizes and
-                thicknesses according to customer's order.
-              </span>
+              <span> صنایع خودرو </span>
             </li>
             <li>
-              <span>
-                Designing and manufacturing all kinds of industrial, special
-                molds and extrusion orders
-              </span>
+              <span> صنایع ساختمان </span>
             </li>
             <li>
-              <span>
-                Casting with tilting furnace by DC method with billet sizes of 7
-                and 8 inches in alloys from 1000 to 7000
-              </span>
+              <span> صنایع لوازم پزشکی </span>
             </li>
             <li>
-              <span> Ingot casting in 1000 to 7000 alloys </span>
+              <span> تجهیزات پزشکی، قطعات صنعتی </span>
+            </li>
+            <li>
+              <span> صنایع گلخانه ای و سازه های چادری </span>
             </li>
           </ul>
         </div>
       </div>
       <div class="visions">
-        <h2>Our Visions</h2>
+        <div class="d-flex justify-content-between align-items-center">
+          <h4>چشم انداز ما<span class="blueLine"></span></h4>
+          <h2 class="mont">Our Visions</h2>
+        </div>
         <p>
-          Peyman Profile Asia Company, with the support of its customers' trust
-          and the benefit of high production capacity and excellent quality,
-          having experienced and knowledge-oriented forces, has been able to
-          have a special position in the aluminum industry as a professional and
-          influential group.
+          شرکت پیمان پروفیل آسیا با پشتوانه اعتماد مشتریان خود و بهره مندی از
+          ظرفیت تولید بالا و کیفیت عالی با داشتن نیروهای مجرب و دانش محور
+          توانسته است به عنوان مجموعه ای حرفه ای و تاثیرگذار جایگاه ویژه ای در
+          صنعت آلومینیوم داشته باشد.
         </p>
-        <p>
-          The goals and future plans of this company are based on the following
-          policies :
-        </p>
+        <p>اهداف و برنامه های آتی این شرکت بر اساس سیاست های زیر است:</p>
         <div class="items">
           <div class="d-flex justify-content-between">
             <div class="item">
               <img src="/icons/smarterIcon1.svg" alt="" />
-              <h3>Equipping and updating machines</h3>
+              <h3>تجهیز و به روز رسانی ماشین آلات</h3>
               <p>
-                Equipping and updating factory machines with the most advanced
-                technology of the day
+                تجهیز و به روز رسانی ماشین آلات کارخانه با پیشرفته ترین تکنولوژی
+                روز
               </p>
             </div>
             <div class="item item2">
               <img src="/icons/smarterIcon2.svg" alt="" />
-              <h3>Promotion of technical knowledge</h3>
-              <p>
-                Continuous improvement of the technical knowledge of our experts
-                and new information
-              </p>
+              <h3>ارتقاء دانش فنی</h3>
+              <p>بهبود مستمر دانش فنی کارشناسان ما و اطلاعات جدید</p>
             </div>
           </div>
           <div class="d-flex justify-content-between">
             <div class="item">
               <img src="/icons/smarterIcon3.svg" alt="" />
-              <h3>Knowing the market of this industry</h3>
+              <h3>دانستن بازار این صنعت</h3>
               <p>
-                Knowing the market of this industry in the international arena
-                and providing the best suggestions and producing special
-                sections to meet the market's needs
+                شناخت بازار این صنعت در عرصه بین المللی و ارائه بهترین پیشنهادات
+                و تولید بخش های ویژه برای رفع نیاز بازار
               </p>
             </div>
             <div class="item item2">
               <img src="/icons/smarterIcon4.svg" alt="" />
-              <h3>New standards of aluminum industry</h3>
+              <h3>استانداردهای جدید صنعت آلومینیوم</h3>
               <p>
-                Acquiring new standards of the aluminum industry in management,
-                production lines, products and customer interaction
+                کسب استانداردهای جدید صنعت آلومینیوم در مدیریت، خطوط تولید،
+                محصولات و تعامل با مشتری
               </p>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="certificates">
+        <div class="padd d-flex justify-content-between align-items-end">
+          <h3>گواهینامه ها <span class="blueLine"></span></h3>
+          <h2 class="mont">Certificates</h2>
+        </div>
+        <p class="padd">
+          شرکت پیمان پروفیل آسیا موفق به دریافت iso ها و پروانه استاندارد ملی
+          شده است.
+        </p>
+        <div class="d-flex images align-items-center">
+          <img src="/icons/arrowRight.svg" alt="" />
+          <div class="d-flex justify-content-between col">
+            <img src="/images/certificates1.jpg" class="image" alt="" />
+            <img src="/images/certificates2.jpg" class="image" alt="" />
+            <img src="/images/certificates3.jpg" class="image" alt="" />
+            <img src="/images/certificates4.jpg" class="image" alt="" />
+          </div>
+          <img src="/icons/arrowLeft.svg" alt="" />
         </div>
       </div>
       <Footer lang="fa" />
