b26913ccceb0a82a46cbe1e63c3791ec5ef44822
alireza <alirezamashayekhy2000@gmail.com>
2023-02-26 21:05:06 +0330

    k

---
+++ pages/industrial.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="blogs">
+    <div class="enBlogs mont" v-if="language == 'en'">
+      <Header lang="eng" @change="change" />
+      <Footer lang="en" />
+    </div>
+    <div class="faBlogs irancell" v-else>
+      <Header lang="fa" @change="change" />
+      <div class="topDiv">
+        <h3 class="mont">Products</h3>
+        <h1>محصولات تولیدی شرکت ما</h1>
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+          از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
+          سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز.
+        </p>
+      </div>
+      <div class="scroll d-flex justify-content-end align-items-center">
+        <div class="scrollIcon">
+          <span class="scrollCircle"></span>
+        </div>
+        <span class="scrollTitle">Scroll Down</span>
+      </div>
+      <div class="lineDiv">
+        <Lines myVw="1920" myVh="100" />
+      </div>
+
+      <Footer lang="fa" />
+    </div>
+    <MouseEffect />
+  </div>
+</template>
+      
+      <script>
+export default {
+  data() {
+    return {
+      language: "",
+    };
+  },
+  mounted() {
+    this.language = this.$store.state.lang;
+  },
+  methods: {
+    change() {
+      this.$store.commit("change");
+    },
+  },
+  watch: {
+    "$store.state.lang"() {
+      this.language = this.$store.state.lang;
+    },
+  },
+};
+</script>
+      
+      <style>
+</style>
